9377c69b8c2bdc426ef152114656ca7a01e8e407
Cal <cwolfson58@gmail.com>
2024-01-27 21:30:55 +1100

    fix build?

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     idea
     java
     id("gg.essential.loom") version "0.10.0.+"
+    id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm") version "1.9.0"
 }
@@ -68,6 +69,9 @@ loom {
         "client" {
             arg("--mods", devenvMod.resolve().joinToString(",") { it.relativeTo(file("run")).path })
         }
+    }
+    forge {
+        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
     }
     runConfigs {
         "client" {
